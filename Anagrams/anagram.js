a6244cb4eb4c615107a0fa1c4bf6e8ed6c72b442
Andres A <andresar11@gmail.com>
2018-08-06 14:18:59 -0400

    Anagram solution in JS

---
+++ Anagrams/anagram.js
@@ -0,0 +1,15 @@
+// Check to see if two provided strings are anagrams of eachother.
+
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
